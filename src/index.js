de83a2826cfed02e639906f16b1469dce08ad09b
KaterynaTesta <84970518+KaterynaTesta@users.noreply.github.com>
2021-11-19 12:22:26 +0400

    added apiService

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+import { NewApiService } from './js/apiService';
+import { searchImages } from 'pixabay-api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const searchForm = document.querySelector('.search-form');
+const submitBtn = document.querySelector('.button');
+searchForm.addEventListener('submit', onSearch);
+
+function onSearch(event) {
+  evt.preventDefault();
+  if (formData !== evt.currentTarget.elements.searchQuery.value) {
+    formData = evt.currentTarget.elements.searchQuery.value;
+    galleryWrap.innerHTML = '';
+    loadPictures(formData);
+  }
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default class NewApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  async fetchPhotos() {
+    const BASE_URL = `https://pixabay.com/api/?key=24397796-604c3a9fc6ff44cceee5653ad&q=${this.searchQuery}&per_page=40&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`;
+    const response = await axios.get(BASE_URL);
+    this.page += 1;
+    return response.data;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+// const API_KEY = '24397796-604c3a9fc6ff44cceee5653ad';
+// const BASE_URL = 'https://pixabay.com/api/';
